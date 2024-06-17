016f886f52d0051c7cd1533335e233bc9e820a2d
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-17 17:20:15 +0800

    feat: 优化体验

--- StapxsQQWebview/StapxsQQWebview.cpp
+++ StapxsQQWebview/StapxsQQWebview.cpp
@@ -8,8 +8,12 @@ int wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LP
 int main(int argc, char **argv)
 {
 #endif
-    webview::webview WebviewObject(true, nullptr);
-    WebviewObject.navigate("https://stapxs.github.io/Stapxs-QQ-Lite-2.0/");
-    WebviewObject.run();
+    webview_t w;
+    w = webview_create(false, nullptr);
+    webview_set_size(w, 1000, 700, WEBVIEW_HINT_NONE);
+    webview_set_title(w, "Test");
+    webview_set_html(w, "set_html ok");
+    webview_navigate(w, "https://stapxs.github.io/Stapxs-QQ-Lite-2.0/");
+    webview_run(w);
     return 0;
 }
