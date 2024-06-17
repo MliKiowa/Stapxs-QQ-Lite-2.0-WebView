7eb4ff9177a3d63c4d58ed787ffcb26eb283a763
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-17 17:14:23 +0800

    feat: 步入能跑阶段
    加入本地文件 优化ws 窗口通知还没做

--- StapxsQQWebview/StapxsQQWebview.cpp
+++ StapxsQQWebview/StapxsQQWebview.cpp
@@ -8,11 +8,8 @@ int wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LP
 int main(int argc, char **argv)
 {
 #endif
-    webview_t w = webview_create(0, NULL);
-    webview_set_title(w, "Basic Example");
-    webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
-    webview_set_html(w, "Thanks for using webview!");
-    webview_run(w);
-    webview_destroy(w);
+    webview::webview WebviewObject(true, nullptr);
+    WebviewObject.navigate("https://stapxs.github.io/Stapxs-QQ-Lite-2.0/");
+    WebviewObject.run();
     return 0;
 }
