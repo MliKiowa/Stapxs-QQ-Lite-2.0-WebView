1c7432ede29e6c277b77e17d6176bb52690839a9
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-17 17:06:43 +0800

    feat: init webview

--- StapxsQQWebview/StapxsQQWebview.cpp
+++ StapxsQQWebview/StapxsQQWebview.cpp
@@ -1,9 +1,18 @@
 #include "Common.h"
+#include "webview/webview.h"
 
 #ifdef _WIN_PLATFORM_
-int  wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+int wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
 #else
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 #endif
-
+    webview_t w = webview_create(0, NULL);
+    webview_set_title(w, "Basic Example");
+    webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
+    webview_set_html(w, "Thanks for using webview!");
+    webview_run(w);
+    webview_destroy(w);
+    return 0;
 }
